0043a06f9e0e809867dbf1399330f7d3b7c1f71d
Armin Blaze <box_for_spam@mail.ru>
2019-03-28 19:33:56 +0300

    53 - Fetch + обработка ошибок

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,15 @@
-getUrl('https://swapi.co/api/people/1/')
+getUrl('https://swapi.co/api/people/0/')
 .then(console.log)
+.catch((err) => {
+	console.error(err)
+})
 
 
 async function getUrl(url) {
 	const response = await fetch(url);
+	
+	if(!response.ok) throw new Error(`Ошибка сервера: ${response.status} ${response.statusText}`)
+	
 	const json = await response.json();
 	return json;
 }
