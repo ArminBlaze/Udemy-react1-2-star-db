a478c83e7eeb440346fd9f2297caf49190e12915
Armin Blaze <box_for_spam@mail.ru>
2019-04-17 19:57:00 +0300

    60 - Показываем заглушку при ошибке сервера
    И инлайним картинку в js файле

--- src/components/RandomPlanet/RandomPlanet.jsx
+++ src/components/RandomPlanet/RandomPlanet.jsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import './RandomPlanet.css';
 import SwapiService from "services/SwapiService";
 import Spinner from "components/Spinner/Spinner";
+import ErrorIndicator from "components/ErrorIndicator/ErrorIndicator";
 
 
 export default class RandomPlanet extends Component {
@@ -10,6 +11,7 @@ export default class RandomPlanet extends Component {
   //добавили свойство loading, пока компонент не получит данные с сервера будет крутиться спиннер
   state = {
     loading: true,
+    error: false,
     planet: {
       population: null,
       rotationPeriod: null,
@@ -32,17 +34,16 @@ export default class RandomPlanet extends Component {
   updatePlanet() {
     let maxPlanetNumber = 19;
     let minPlanetNumber = 2;
-    let id = generateRandom(minPlanetNumber, maxPlanetNumber);
+    // let id = generateRandom(minPlanetNumber, maxPlanetNumber);
+    let id = 12555;
     console.log(id);
-
 
     //получаем с сервера планету через сервис для работы с сервером
     this.swapiService.getPlanet(id)
-    .then( (planet) => {
-      console.log(planet);
-
-      //когда данные загружены обновляем планету и меняем флаг загрузки, чтобы спрятать спиннер
-      this.setState({planet, loading: false});
+    .then(this.onPlanetLoaded)
+    .catch(this.onError)
+    .finally( () => {
+      this.setState({loading: false})
     })
 
 
@@ -53,15 +54,27 @@ export default class RandomPlanet extends Component {
     }
   }
 
+  onError = (err) => {
+    console.log(err);
+    this.setState({error: true})
+  }
+
+  onPlanetLoaded = (planet) => {
+    console.log(planet);
+    //когда данные загружены обновляем планету и меняем флаг загрузки, чтобы спрятать спиннер
+    this.setState({planet});
+  }
+
   render() {
     //планета теперь отдельный объект-свойство, поэтому у нас получается короткая деструктуризация
-    const {planet, loading} = this.state;
+    const {planet, loading, error} = this.state;
 
     //если данные получены, то спиннер можно спрятать
     //также вся разметка компонента вынесена в отдельный компонент, для удобства
     return (
       <div className="RandomPlanet jumbotron rounded">
         {loading ? <Spinner /> : null}
+        {error ? <ErrorIndicator /> : null}
         <PlanetView planet={planet}/>
       </div>
     );
